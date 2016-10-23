c48262203e63fc45622a9d5617afea3a9c53820c
Travis Gorman <thefarbetween@me.com>
2016-10-22 19:18:37 -0500

    init repo

---
+++ app/scripts/settings.js
@@ -0,0 +1,6 @@
+export default {
+  url: `http://baas.kinvey.com/`
+  appKey: 'kid_r1Ek_OFkl',
+  appSecret: 'eafe8b7567b546d3897de4e20debbdf7',
+  basicAuth: `Basic ${btoa('kid_r1Ek_OFkl:eafe8b7567b546d3897de4e20debbdf7')} `
+}
---
+++ dist/main.js
@@ -0,0 +1,1 @@
+console.error("SyntaxError: /Users/travisgorman/Desktop/$/pokebae/app/scripts/settings.js: Unexpected token, expected , (3:2) while parsing file: /Users/travisgorman/Desktop/$/pokebae/app/scripts/settings.js");
